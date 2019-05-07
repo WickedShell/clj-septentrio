fdeb62f7fb1c2f4160ac4abd78bdaf34cb029a72
Michael du Breuil <wicked.shell.scripts@gmail.com>
2019-05-06 22:18:11 -0700

    Release 0.1.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-septentrio "0.1.1-SNAPSHOT"
+(defproject clj-septentrio "0.1.1"
   :description "Septentrio binary protocol parser"
   :url "http://github.com/wickedshell/clj-septentrio"
   :license {:name "Eclipse Public License"
